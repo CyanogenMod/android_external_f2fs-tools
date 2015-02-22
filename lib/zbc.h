eb8a892e21cecc478a346e7c16f1a2683f0a2df0
Christopher N. Hesse <raymanfx@gmail.com>
2015-02-22 15:43:59 +0100

    f2fs-tools: extract linux type definitions to local header
    This allows for building f2fs-tools on non-linux platforms.
    The patch was taken from official OpenWrt trunk releases.
    
    * luca020400: port to f2fs-tools
    
    Change-Id: I7dd1c16baff597debcf2dc8c0475c5c14e9de29d

--- lib/zbc.h
+++ lib/zbc.h
@@ -23,7 +23,11 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <inttypes.h>
+#ifdef __linux__
 #include <linux/types.h>
+#else
+#include "linux_types.h"
+#endif
 #include <sys/types.h>
 #include <sys/ioctl.h>
 #include <scsi/scsi.h>
