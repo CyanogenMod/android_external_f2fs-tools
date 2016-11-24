0d1874710ed7609fa1c6b275f0f61a3b1389095e
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-11-24 22:05:46 +0100

    libf2fs_fmt: Add missing src file
    Change-Id: Ibb642ca0e12b3c7ad3f83dd5c4b76707c0717a93

--- Android.mk
+++ Android.mk
@@ -88,6 +88,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := libf2fs_fmt-host
 LOCAL_SRC_FILES := \
     lib/libf2fs.c \
+    lib/zbc.c \
     mkfs/f2fs_format.c \
     mkfs/f2fs_format_utils.c \
 
@@ -102,6 +103,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := libf2fs_fmt_host_dyn
 LOCAL_SRC_FILES := \
     lib/libf2fs.c \
+    lib/zbc.c \
     mkfs/f2fs_format.c \
 
 LOCAL_C_INCLUDES := $(common_C_INCLUDES)
